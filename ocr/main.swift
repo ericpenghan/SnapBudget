6c019b3f8f2772c7458fe035237847472e808880
Jarren Tay <jarrentayusa@gmail.com>
2019-09-28 12:40:59 -0400

    Add comments. Changed Totalling regex. Regexes could be wrong
    Needs work:
    Company name identifying is not quite right
    Date identifying is not quite right

--- ocr/main.swift
+++ ocr/main.swift
@@ -1,6 +1,8 @@
 import Foundation
 
+// Don't change the API key
 let apiKey = "f0d82f3f1a88957"
+// There are receipt0_0.jpg to receipt5_0.jpg. receipt2_0.jpg is the asian market that does not work well.
 let url = "https://raw.githubusercontent.com/JarrenTay/test/master/receipt5_0.jpg"
 
 callOCRSpace(apiKey: apiKey, url: url)
--- ocr/ocrReceipt.swift
+++ ocr/ocrReceipt.swift
@@ -70,12 +70,14 @@ let _apiKey = "f0d82f3f1a88957"
 let _url = "https://raw.githubusercontent.com/JarrenTay/test/master/receipt5_0.jpg"
 
 func callOCRSpace(apiKey: String, url: String) {
-    // prepare json data
     var estimatedTotal = 0.0
     var date = ""
     var company = ""
 
     // create get request
+    // The OCR API is located here: https://ocr.space/ocrapi
+    // Overlay describes the position of a line of words, what that line of words is composed of, and how many lines.
+    // Reference json/receipt0_0_Overlay.json for an example output of the api
     let url = URL(string: "https://api.ocr.space/parse/imageurl?apikey=" + apiKey + "&url=" + url + "&isoverlayrequired=true")!
     var request = URLRequest(url: url)
     request.httpMethod = "GET"
@@ -85,18 +87,33 @@ func callOCRSpace(apiKey: String, url: String) {
             print(error?.localizedDescription ?? "No data")
             return
         }
+        // totalList describes the prices that are in the same y-position as a line that contains "total" or "balance" but not "subtotal" or "saving"
+        // Prices lower on the receipt are scored higher because it's probably more likely to be the value we are interested in.
         var totalList: [TotalScores] = []
         do {
+            // Interpret API output
             let product = try decoder.decode(OcrData.self, from: data)
+
+            // lineList is a list of Line objects, reduces the amount of information we're working with because I didn't think it was necessary?
+            // Maybe we don't have to make a lineList.
             var lineList: [Line] = []
+
+            // totalTopList is a list of y values of lines that contain "total"-ish
             var totalTopList: [Int] = []
             for line in product.ParsedResults[0].TextOverlay.Lines {
                 lineList.append(Line(inTop: line.MinTop, inText: line.LineText))
+
+                // We remove spaces for this comparison because sometimes spaces occur in the middle of to tal and it messes us up.
                 let lineSimp = line.LineText.lowercased().replacingOccurrences(of: " ", with: "")
+                // subtotal and saving(total savings) are typically not what we are looking for
                 if((lineSimp.contains("total") || lineSimp.contains("balance")) && !lineSimp.contains("subtotal") && !lineSimp.contains("saving")) {
                     totalTopList.append(line.MinTop)
                 }
+
+                // Try to determine date if it has not been determined yet
                 if (date == "") {
+                    // Regex attempting to recognize anything between 09/12/09 to 9/2/2019 to 125436 05/02/10 24325
+                    // not working properly
                     if (lineSimp.range(of: "^.*[0-9][0-9]*\\/[0-9][0-9]*\\/[0-9][0-9][0-9]*.*$", options: .regularExpression, range: nil, locale: nil) != nil) {
                         let lineSimpArr = lineSimp.components(separatedBy: "/")
                         let lineSimp1 = lineSimpArr[0]
@@ -116,6 +133,7 @@ func callOCRSpace(apiKey: String, url: String) {
                         if (lineSimp2.count == 2 || lineSimp2.count == 1) {
                           date2 = lineSimp2
                         } else {
+                          // If there are 3 numbers between the /, we are in trouble
                           date = "ERROR"
                         }
                         if lineSimp3.count >= 2 {
@@ -124,9 +142,11 @@ func callOCRSpace(apiKey: String, url: String) {
                             date3 = String(lineSimp3.prefix(1))
                           }
                         } else {
+                          // If there are less than 2 numbers to the left of the right /, we are in trouble 
                           date = "ERROR"
                         }
                         if date != "ERROR" {
+                          // Try and create 
                           date = date1 + "/" + date2 + "/" + date3
                         }
                     }
@@ -134,9 +154,11 @@ func callOCRSpace(apiKey: String, url: String) {
 
             }
             print(totalTopList)
+            // Sort lines by their verticality, high lines are earlier
             lineList = lineList.sorted(by: { $0.top < $1.top })
             var notFoundCompany = true
             var count = 0
+            // Try to find the company name. if the string is < 70% characters, its probably not a name
             while notFoundCompany {
                 var numChar = 0
                 var numDig = 0
@@ -156,11 +178,13 @@ func callOCRSpace(apiKey: String, url: String) {
                 count = count + 1
             }
 
+            // Figure out the prices of each total
             for total in totalTopList {
                 for line in lineList {
                     if(abs(line.top - total) < 20) {
                         if (line.text.count >= 4) {
-                            if line.text.range(of: "^-?\\$?[0-9]+(,|.)[0-9][0-9]$", options: .regularExpression, range: nil, locale: nil) != nil {
+                            // Regex that accepts -$3.00 or 2.49 or 748,00 or $2202.00 
+                            if line.text.range(of: "^-?\\$?[0-9][0-9]*(,|.)[0-9][0-9]$", options: .regularExpression, range: nil, locale: nil) != nil {
                                 var cost = line.text.replacingOccurrences(of: ",", with: ".")
                                 cost = cost.replacingOccurrences(of: "$", with: "")
                                 totalList.append(TotalScores(inScore: line.top - abs(line.top - total), inTotal: Double(cost)!))
@@ -172,6 +196,7 @@ func callOCRSpace(apiKey: String, url: String) {
         } catch {
             print("error")
         }
+        // Try and figure out which total we want
         var maxScore = 0
         for total in totalList{
             if (total.score > maxScore) {
