90bfc2ac65c691e6969cccc91240af3626dacf27
ericpenghan <47765216+ericpenghan@users.noreply.github.com>
2019-09-28 13:43:57 -0400

    Add files via upload

---
+++ RecipetClass.swift
@@ -0,0 +1,30 @@
+//
+//  RecipetClass.swift
+//  SnapBudget
+//
+//  Created by Eric Han on 9/28/19.
+//  Copyright © 2019 Eric Han. All rights reserved.
+//
+
+import UIKit
+
+class RecipetClass: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ViewController.swift
+++ ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//make it global, so all storyboard can access to it 
+private var imagePicker: UIImagePickerController!
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {//last 2 UI classes will let you take pic and use the data
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  
+  //take a photo
+  @IBAction func takePic(_ sender: Any) {
+    imagePicker = UIImagePickerController()
+    imagePicker.delegate = self//make it control the whole class so it can control the camrea
+    
+    //if the camra is avaiavle then do these
+    if UIImagePickerController.isSourceTypeAvailable(.camera){
+      imagePicker.sourceType = .camera
+    }
+    else{
+      //if not set it to photolibiary
+      imagePicker.sourceType = .photoLibrary
+      print("not woreking")
+    }
+    imagePicker.allowsEditing = true//allow user to edit
+    imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: imagePicker.sourceType)!
+    
+    
+    self.present(imagePicker, animated: true, completion: nil);//it will allow the user to see the camra
+  }
+  
 
+  
+  
+  
 }
-
